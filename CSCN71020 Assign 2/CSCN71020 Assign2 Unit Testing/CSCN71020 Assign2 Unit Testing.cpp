a131724443f47f3626b720c8b394ae03971e3535
Eric Weng <Eweng3376@conestogac.on.ca>
2021-10-17 22:20:06 -0400

    Completed Task #3/#4. Fixed all bad unit testing code and mistakes in function code.

--- CSCN71020 Assign 2/CSCN71020 Assign2 Unit Testing/CSCN71020 Assign2 Unit Testing.cpp
+++ CSCN71020 Assign 2/CSCN71020 Assign2 Unit Testing/CSCN71020 Assign2 Unit Testing.cpp
@@ -1,10 +1,10 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
-extern "C" int getPerimeter(int, int);
-extern "C" int getArea(int, int);
-extern "C" int setLength(int, int);
-extern "C" int setWidth(int, int);
+extern "C" int getPerimeter(int* length, int* width);
+extern "C" int getArea(int* length, int* width);
+extern "C" int setLength(int input, int* length);
+extern "C" int setWidth(int input, int* width);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -18,60 +18,75 @@ namespace CSCN71020Assign2UnitTesting
 		TEST_METHOD(perimeterFunctionality) {
 			//This is testing the getPerimeter function using length 4, width 3 to get 4+4+3+3=14
 			int Result = 0;
+			int length = 4;
+			int width = 3;
 
-			Result = getPerimeter(4, 3);
+			Result = getPerimeter(&length, &width);
 			Assert::AreEqual(14, Result);
 		}
 
 		TEST_METHOD(areaFunctionality) {
 			//This is testing the getArea function using length 4, width 3 to get 4*3=12
 			int Result = 0;
-			Result = getArea(4, 3);
+			int length = 4;
+			int width = 3;
+			Result = getArea(&length, &width);
 			Assert::AreEqual(12, Result);
 		}
 
 
 		TEST_METHOD(lengthFunctionality1) {
-			//This is testing setLength for length = 5
-			int Result = 0;
-			Result = setLength(5, 1);
-			Assert::AreEqual(5, Result);
+			//This is testing setLength for length = 1 and input = 5
+
+			int length = 1;
+			int input = 5;
+			setLength(input, &length);
+			Assert::AreEqual(5, length);
 
 		}
 
 		TEST_METHOD(lengthFunctionality2) {
-			//This is testing setLength for length = 0
-			int Result = 0;
-			Result = setLength(0, 5);
-			Assert::AreEqual(5, Result);
+			//This is testing setLength for length = 5 and input = 0
+			int length = 5;
+			int input = 0;
+			setLength(input, &length);
+			Assert::AreEqual(5, length);
 
 		}
 
 		TEST_METHOD(lengthFunctionality3) {
-			//This is testing setLength for length = 100
-			int Result = 0;
-			Result = setLength(100, 5);
-			Assert::AreEqual(5, Result);
+			//This is testing setLength for length = 5 and input = 100
+
+			int length = 5;
+			int input = 100;
+			setLength(input, &length);
+			Assert::AreEqual(5, length);
 		}
 		TEST_METHOD(widthFunctionality1) {
-			//This is testing setWidth for width = 3
-			int Result = 0;
-			Result = setWidth(3, 1);
-			Assert::AreEqual(3, Result);
+			//This is testing setWidth for width = 1 and input = 300
+
+			int width = 1;
+			int input = 300;
+			setWidth(input, &width);
+			Assert::AreEqual(1, width);
 
 		}
 		TEST_METHOD(widthFunctionality2) {
-			//This is testing setWidth for width = 300
-			int Result = 0;
-			Result = setWidth(300, 3);
-			Assert::AreEqual(3, Result);
+			//This is testing setWidth for width = 1 and input = 3
+
+			int width = 1;
+			int input = 3;
+			setWidth(input, &width);
+			Assert::AreEqual(3, width);
 
 		}
 		TEST_METHOD(widthFunctionality3) {
-			//This is testing setWidth for width = -30
-			int Result = 0;
-			Result = setLength(-30, 3);
-			Assert::AreEqual(3, Result);
+			//This is testing setWidth for width = 3 and input = -30
+
+			int width = 3;
+			int input = -30;
+			setLength(input, &width);
+			Assert::AreEqual(3, width);
 		}
 	};
 }
